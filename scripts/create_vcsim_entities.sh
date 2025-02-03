49e23ebc5c5aa70d94d096f4fa62b174819d2fa4
paul.guo <paul.guo@nutanix.com>
2025-02-03 22:00:45 +0000

    bug fix

--- scripts/create_vcsim_entities.sh
+++ scripts/create_vcsim_entities.sh
@@ -8,8 +8,8 @@ echo -e "$pline cluster count: $1 $pline"
 echo -e "$pline node count per cluster: $2 $pline"
 echo -e "$pline vm count per node: $3 $pline"
 echo -e "$pline killing existing vcsim proc $pline"
-sudo pkill vcsim
-sudo /home/nutanix/go/bin/vcsim -l $ip:8989 -username $VCSIM_USERNAME -password $VCSIM_PASSWORD &
+pkill vcsim
+/go/bin/vcsim -l $ip:8989 -username $VCSIM_USERNAME -password $VCSIM_PASSWORD &
 # shellcheck disable=SC2006
 newpid=`ps -aef | grep vcsim | grep -v sh | grep -v grep | awk '{print $2}'`
 export GOVC_URL=https://$VCSIM_USERNAME:$VCSIM_PASSWORD@$ip:8989/sdk GOVC_SIM_PID=$newpid
@@ -23,27 +23,26 @@ for ((cluster=1; cluster<=$1; cluster++))
 do
   cluster_name="${CLUSTER_NAME_PREFIX}${cluster}_${ip}"
   echo -e "$pline Creating $cluster_name $pline"
-  govc cluster.create $cluster_name
+  /go/bin/govc cluster.create $cluster_name
   for ((node=1; node<=$2; node++))
   do
     host_name=$(printf "%d.%d.%d.%d\n" "$((RANDOM % 256 ))" "$((RANDOM % 256 ))" "$((RANDOM % 256 ))" "$((RANDOM % 256 ))")
     echo -e "$pline Adding host $host_name in $cluster_name $pline"
-    govc cluster.add -cluster=$cluster_name -hostname $host_name -username $VCSIM_USERNAME -password $VCSIM_PASSWORD -k=true
+    /go/bin/govc cluster.add -cluster=$cluster_name -hostname $host_name -username $VCSIM_USERNAME -password $VCSIM_PASSWORD -k=true
     datastore_name="LocalDS_$host_name"
     echo -e "$pline Creating $datastore_name for $host_name $pline"
-    govc datastore.create -type local -name $datastore_name -path /var/local $host_name
+    /go/bin/govc datastore.create -type local -name $datastore_name -path /var/local $host_name
     echo -e "$pline Adding host $host_name to DVS0 $pline"
-    govc dvs.add -dvs=/DC0/network/DVS0 -k=true $host_name
+    /go/bin/govc dvs.add -dvs=/DC0/network/DVS0 -k=true $host_name
     for ((vm=1; vm<=$3; vm++))
     do
       vm_name="${host_name}_VM_${vm}"
       echo -e "$pline Creating $vm_name $pline"
-      govc vm.create -ds $datastore_name -pool=/DC0/host/$cluster_name/Resources -host $host_name -net="VM Network" -on=false $vm_name
+      /go/bin/govc vm.create -ds $datastore_name -pool=/DC0/host/$cluster_name/Resources -host $host_name -net="VM Network" -on=false $vm_name
       vm_ip=$(printf "%d.%d.%d.%d\n" "$((RANDOM % 256 ))" "$((RANDOM % 256 ))" "$((RANDOM % 256 ))" "$((RANDOM % 256 ))")
       vm_mac=$(printf "00:60:2f$(od -txC -An -N3 /dev/random|tr \  :)")
-      govc vm.customize -vm $vm_name -mac $vm_mac -ip $vm_ip -netmask 255.255.255.0
-      govc vm.power -on $vm_name
+      /go/bin/govc vm.customize -vm $vm_name -mac $vm_mac -ip $vm_ip -netmask 255.255.255.0
+      /go/bin/govc vm.power -on $vm_name
     done
   done
 done
-
